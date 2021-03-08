3cd376a4faeb6dc5530896d9b79241e331791947
Diego Diez <diego10ruiz@gmail.com>
2021-03-08 14:58:21 +0900

    Initial code commit.

---
+++ R/CanekDebug-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @import Canek Seurat ggplot2 tidyr dplyr
+#' @importFrom ComplexHeatmap Heatmap
+#' @importFrom stats time
+#' @importFrom ggrepel geom_text_repel
+## usethis namespace: end
+NULL
