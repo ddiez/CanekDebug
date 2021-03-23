23ef6fd083f3d9fbe0f429588eacd6798f61954d
Diego Diez <diego10ruiz@gmail.com>
2021-03-23 17:08:42 +0900

    Add missing tibble dependency.

--- R/CanekDebug-package.R
+++ R/CanekDebug-package.R
@@ -4,7 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @import Canek Seurat ggplot2 tidyr dplyr
+#' @import Canek Seurat ggplot2 tidyr dplyr tibble
 #' @importFrom ComplexHeatmap Heatmap
 #' @importFrom stats time
 #' @importFrom ggrepel geom_text_repel
